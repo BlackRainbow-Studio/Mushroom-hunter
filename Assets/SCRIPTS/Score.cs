74cee1b19721e2a6e201d25457ea3068acd2597b
JaisyKo-ZenBook <jaisyko@gmail.com>
2022-12-04 16:12:29 +0300

    Added: Started "item generation" block
    -> Созданы папки по типам ассетов
    + Генерация итема рандомно по оси x

---
+++ Assets/SCRIPTS/Score.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private int score = 0;
+
+    [SerializeField] TMPro.TMP_Text text;
+
+    public void Increase()
+    {
+        Debug.Log("Score increased");
+
+        score += 5;
+        text.text = score.ToString();
+    }
+    
+    public void Decrease()
+    {
+        Debug.Log("Score decreased");
+
+        score -= 10;
+        text.text = score.ToString();
+    }
+}
