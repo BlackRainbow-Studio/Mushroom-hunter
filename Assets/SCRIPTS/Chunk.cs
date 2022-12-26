b47a7c5bf4c78e7a245b38c571a08986076e7163
JaisyKo-ZenBook <jaisyko@gmail.com>
2022-12-26 19:17:10 +0300

    Added: penalty for hitting

--- Assets/SCRIPTS/Chunk.cs
+++ Assets/SCRIPTS/Chunk.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class Chunk : MonoBehaviour
 {
     [SerializeField] public Transform endPoint;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Debug.Log("Мимо!");
+        Score.Instance.Decrease();
     }
 }
