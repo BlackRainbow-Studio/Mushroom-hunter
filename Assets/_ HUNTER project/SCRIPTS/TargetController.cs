f8508144835e93f2b50a8d4cb4f64f9d4383bd6d
JaisyKo-ZenBook <jaisyko@gmail.com>
2023-01-24 17:26:57 +0300

    Added: Target horizontal moving

--- Assets/_ HUNTER project/SCRIPTS/TargetController.cs
+++ Assets/_ HUNTER project/SCRIPTS/TargetController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,26 @@ using UnityEngine;
 public class TargetController : MonoBehaviour
 {
     [SerializeField] float forwardSpeed;
+    
+    [SerializeField] float horizontalSpeed;
+    Vector3 horizontalDirection = Vector3.right;
 
     [SerializeField] GameEvent_SO TargetReached;
 
     void Update()
     {
         MoveVertical();
+        MoveHorizontal();
     }
     private void MoveVertical()
     {
         transform.Translate(Vector3.forward * forwardSpeed);
+    }
+    private void MoveHorizontal() 
+    {
+        if (Math.Abs(transform.position.x) > 3)
+            horizontalDirection *= -1;
+        transform.Translate(horizontalDirection * horizontalSpeed);
     }
 
     public void SetSpeed() { }
